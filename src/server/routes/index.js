01235909a5a91958e0547d52d8bb35d843c34e05
ilci66 <akbiyik.ilker7@gmail.com>
2021-12-21 13:06:35 +0100

    new routes & objectives

--- src/server/routes/index.js
+++ src/server/routes/index.js
@@ -9,13 +9,26 @@ const { Drawing, Object, Drawing_Object } = require('../../db/models.js');
 
 dbConnectionTest();
 
+
+router.get('/', async (req, res, next) => {
+  res.send('It works')
+})
+
 router.get('/drawings', async (req, res, next) => {
 
   let allDrawings = await Drawing.findAll({})
   res.status(200).json(allDrawings)
 });
 
+router.get('drawing/:id', async (req, res, next) => {
+  console.log(req.query.params);
+  res.send("get the drawing")
+});
 
+router.put('drawing/:id', async (req, res, next) => {
+  console.log(req.query.params);
+  res.send("update the object types in the drawings")
+})
 
 // router.all('*', (req, res, next) =>
 //   res.status(404).json({
