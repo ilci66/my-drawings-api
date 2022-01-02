a30ea0dce5686245dbece2aececa319be5af9832
ilci66 <akbiyik.ilker7@gmail.com>
2022-01-02 13:17:21 +0100

    added transactions

--- src/server/routes/index.js
+++ src/server/routes/index.js
@@ -24,30 +24,40 @@ router.get('/drawing/:id', async (req, res, next) => {
   res.send("get the drawing")
 });
 
+router.delete('/drawing/:id', async (req, res, next) => {
+  // console.log(req.query.params);
+  res.send("delete the drawing")
+});
+
+// this was supposed to be put btut i don't wanna add the functionaity to add drawings here
 router.post('/drawing/:id', async (req, res, next) => {
+
+  const t = await sequelize.transaction();
+
   console.log("PUT params ===>",(req.params), "body ===>",req.body);
-  // res.json({message:"update the object types in the drawings"})
-  // drawinginstance.setobjects() will be used herebut not really sure how exactly
   
   try{  
     let drawing = await Drawing.findOne({where: {id: req.params.id}})
-
     if(req.body.length === 2){ 
       let types = await  Object.findAll({where: { id: { [Op.or]:  [req.body[0].id, req.body[1].id] }}}) 
-      await drawing.setObjects(types)
+      await drawing.setObjects(types, { transaction: t })
       // console.log("length is 2 and sending drawing ==>", drawing)
+      await t.commit();
       await drawing.save()
       return res.status(200).json(drawing)
     }
     else{ 
     // else if(req.body.length === 1) { 
       let types = await  Object.findAll({where: { id: req.body[0].id }}) 
-      await drawing.setObjects(types)
+      await drawing.setObjects(types, { transaction: t })
       // console.log("length is not 2 and sending drawing ==>",drawing)
+      await t.commit();
+
       await drawing.save()
       return res.status(200).json(drawing)
     }
   }catch(e) { 
+    await t.rollback();
     console.log("error occured while setting type ==> ", e)
     return res.status(400).json({error: e})
   } 
